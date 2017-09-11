c3795b9ad8f10ab81130da25d0a60c7433fb63bd
Scott Ouellette <scottx611x@gmail.com>
2017-09-10 20:52:01 -0400

    Add HelloWorld class

---
+++ src/HelloWorld.java
@@ -0,0 +1,15 @@
+public class HelloWorld {
+    public static void main( String[] args ) {
+        String message = "Hello World";
+        try {
+            message = String.format("Hello %s", args[0]);
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.err.println("No username provided, using default message.");
+        }
+        finally {
+            System.out.println(message);
+            System.exit( 0 );
+        }
+    }
+}
